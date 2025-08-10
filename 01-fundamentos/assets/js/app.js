39d8f46b56bc54052b6726b1dc6dc60f9a69c721
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-08-09 21:20:11 -0600

    first commit

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,44 @@
+
+// console.log( miNuevoNombre + ' dev' );
+
+// alert('Hola desde app.js')
+
+// console.log( console.log('Hola Mundo') );
+
+// Variables
+
+let a = 10;
+// var b = 10;
+// const c = 10;
+
+// c = 20; // No se puede cambiar el valor
+// a =  20;
+// b = 30;
+
+// c = 30;
+
+let b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log('%c Mis variables', 'color: blue; font-weight: bold'); // Estilo del texto
+// console.log( 'a', a );
+// console.log( { b } );
+// console.log( { c } );
+// console.warn( x );
+// console.error( x );
+
+c = 'Hola de nuevo'
+
+console.table([a, b, c, d, x]);
+
+// var puede sobreescribir las varibles globales
+// var outerWidth = 10000;
+// let outerWidth = 1000000;
+// const outerHeight = 679;
+
+// var miNuevoNombre = 'Eduardo Caballero';
+// let miNuevoNombre = 'Eduardo Caballero';
