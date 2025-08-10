ad49f76ac8737cd9491044aa6fd476342950601e
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-08-09 21:37:17 -0600

    fundamentos

--- 01-fundamentos/assets/js/alerts.js
+++ 01-fundamentos/assets/js/alerts.js
@@ -1,1 +1,16 @@
 // console.log( miNombre );
+
+// Promt, confirm y alert
+
+// Muestra un mensaje simple al usuario.
+// alert('Hola Mundo');
+
+// Solicita al usuario que ingrese un valor, devolviendo el texto ingresado o null.
+// let nombre = prompt('¿Cual es tu nombre?');
+// console.log(nombre);
+// console.log( '***' + nombre + '***' );
+
+
+// Muestra un mensaje con opciones "Aceptar" y "Cancelar", devolviendo true o false.
+const seleccion = confirm('¿Estas seguro de eliminarlo?');
+console.log(seleccion);
