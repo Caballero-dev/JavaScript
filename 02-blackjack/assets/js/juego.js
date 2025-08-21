cc735e1601b6c85cacd46c73b43356e353e018b0
Caballero-dev <153244973+Caballero-dev@users.noreply.github.com>
2025-08-21 17:54:05 -0600

    Mejora y optimización del código

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,178 +1,177 @@
-/**
- * 2C = Two of Clubs
- * 2D = Two of Diamonds
- * 2H = Two of Hearts
- * 2S = Two of Spades
- */
 
-let deck = [];
-const types = ['C', 'D', 'H', 'S'];
-const specials = ['A', 'J', 'Q', 'K'];
+/* ------ Sintaxis del patron modulo ------ */
 
-let playerPoints = 0,
-    computerPotins = 0;
+// A continuación, se utiliza una función anónima autoejecutada (IIFE, Immediately Invoked Function Expression).
+// Estas funciones se definen y se ejecutan inmediatamente, creando un nuevo ámbito (scope).
+// Al no tener un identificador (nombre), no es posible llamarlas directamente después de su ejecución.
+// Esto ayuda a encapsular el código y evitar la contaminación del espacio global de nombres.
 
-// Referencias de HTML
-const btnNew = document.querySelector('#btnNew');
-const btnRequest = document.querySelector('#btnRequest');
-const btnStop = document.querySelector('#btnStop');
+// (function() {
+// })()
 
-const txtPoints =  document.querySelectorAll('small');
-const divPlayerCards = document.querySelector('#player-cards');
-const divComputerCards = document.querySelector('#computer-cards');
+const myModule = (() => {
+  // Activamos el modo estricto de JavaScript con 'use strict'.
+  // Esto obliga a escribir código más limpio y seguro, evitando errores comunes.
+  'use strict';
 
-/* ------ Crear baraja ------ */
-const createDeck = () => {
+  let deck = [];
+  const types = ['C', 'D', 'H', 'S'],
+        specials = ['A', 'J', 'Q', 'K'];
 
-  for( let i = 2; i <= 10; i++ ) {
-    for ( let type of types) {
-      deck.push( i + type );
+  let playersPoints = [];
+
+  // Referencias de HTML
+  const btnNew = document.querySelector('#btnNew'),
+        btnRequest = document.querySelector('#btnRequest'),
+        btnStop = document.querySelector('#btnStop');
+
+  const txtPoints =  document.querySelectorAll('small'), 
+        divPlayersCards = document.querySelectorAll('.divCards');
+
+  /* ------ Crear baraja ------ */
+  const createDeck = () => {
+    deck = [];
+
+    for( let i = 2; i <= 10; i++ ) {
+      for ( let type of types) {
+        deck.push( i + type );
+      }
     }
-  }
 
-  for( let type of types ) {
-    for ( let special  of specials) {
-      deck.push( special + type );
+    for( let type of types ) {
+      for ( let special  of specials) {
+        deck.push( special + type );
+      }
     }
+
+    // Mezclar baraja
+    deck = _.shuffle(deck);
   }
 
-  // Mezclar baraja
-  deck = _.shuffle(deck);
-}
+  /* ------ Inicializa el juego ------ */
+  const startGame = ( numPlayers =  2 ) => {
+    createDeck();
 
-createDeck();
+    playersPoints = [];
+    for ( let i = 0; i < numPlayers; i++ ) {
+      playersPoints.push(0);
+    }
 
-/* ------ Pedir una carta ------ */
-const requestCard = () => {
 
-  if ( deck.length === 0 ) {
-    // throw: se utiliza para lanzar una excepción o un error de manera intencional. 
-    // Esto detiene la ejecución del programa en ese punto.
-    throw 'No hay cartas'
+    txtPoints.forEach( elem => elem.innerText = 0 );
+    divPlayersCards.forEach( elem => elem.innerHTML = '' );
+
+    btnRequest.disabled = false;
+    btnStop.disabled = false;
+
   }
 
-  return deck.pop();
-}
+  /* ------ Pedir una carta ------ */
+  const requestCard = () => {
+    if ( deck.length === 0 ) {
+      throw 'No hay cartas en la baraja';
+    }
+    return deck.pop();
+  }
 
+  /* ------ Obtener valor de la carta ------ */
+  const cardValue = ( card ) => {
+    const value = card.substring(0, card.length - 1);
+    return ( isNaN(value) )
+            ? ( ( value === 'A' ) ? 11 : 10 )
+            : Number(value);
+  }
 
-/* ------ Obtener valor de la carta ------ */
-const cardValue = ( card ) => {
+  /* ------ Acomular puntos a cada jugador ------ */
+  // Turno 0 = Primer jugador y último será la computador 
+  const accumulatePoints = ( card, playerTurn ) => {
+    playersPoints[playerTurn] = playersPoints[playerTurn] + cardValue( card );
+    txtPoints[playerTurn].innerText = playersPoints[playerTurn];
+    return playersPoints[playerTurn];
+  }
 
-  // substring(start, end): este método extrae una parte de un string.
-  // start: La posición inicial (incluida).
-  // end: La posición final (no incluida).
-  const value = card.substring(0, card.length - 1);
+  const createCard = ( cardName, playerTurn ) => {
+      const imgCard = document.createElement('img');
+      imgCard.src = `assets/cartas/${ cardName }.png`;
+      imgCard.classList.add('cards');
+      divPlayersCards[playerTurn].append( imgCard );
+  }
 
-  return ( isNaN(value) ) 
-          ? ( ( value === 'A' ) ? 11 : 10 ) 
-          : Number(value);  
+  const determineWinner = () => {
+    const [ playerPoints, computerPotins ] = playersPoints;
+    setTimeout(()=> {
   
-  /*   
-  let points = 0;
-
-  // isNaN(value): Verifica si un valor no es un número.
-  // Devuelve true si el valor no puede convertirse a un número, y false si sí puede.
-  if ( isNaN(value) ) {
-    points = ( value === 'A' ) ? 11 : 10;
-  } else {
-    // Number: Convierte un string o valor a un número.
-    // Si no puede convertirlo, devuelve NaN (Not-a-Number).
-    points = Number(value);
+      if ( playerPoints > computerPotins && playerPoints <= 21 ) {
+        alert('Jugador gana :)');
+      } else if ( playerPoints <= 21 && computerPotins > 21 ) {
+        alert('Jugador gana :) :)');
+      } else if ( computerPotins > playerPoints && computerPotins <= 21 ) {
+        alert('Computadora gano :(');
+      } else if ( computerPotins <= 21 && playerPoints > 21 ) {
+        alert('Computadora gano :( :(');
+      } else {
+        // console.log( 'test', { computerPotins, playerPoints });
+        alert('Nadie ha ganado >_<');
+      }
+    }, 500 )
   }
-  console.log( points ); 
-  */
-}
 
-/* ------ Realizar turno de la computadora ------ */
-const computerTurn = ( minPoints ) => {
-  do {
-    const card = requestCard();
-    computerPotins = computerPotins + cardValue( card );
-    txtPoints[1].innerText = computerPotins;
-
-    const imgCard = document.createElement('img');
-    imgCard.src = `assets/cartas/${ card }.png`;
-    imgCard.classList.add('cards');
-
-    divComputerCards.append( imgCard );
-
-    if ( minPoints > 21 ) {
-      break;
-    }
+  /* ------ Realizar turno de la computadora ------ */
+  const computerTurn = ( minPoints ) => {
     
-  } while ( (computerPotins < minPoints) && (minPoints <= 21) );
+    let computerPotins = 0;
 
-  setTimeout(()=> {
- 
-    if ( playerPoints > computerPotins && playerPoints <= 21 ) {
-      alert('Jugador gana :)');
-    } else if ( playerPoints <= 21 && computerPotins > 21 ) {
-      alert('Jugador gana :) :)');
-    } else if ( computerPotins > playerPoints && computerPotins <= 21 ) {
-      alert('Computadora gano :(');
-    } else if ( computerPotins <= 21 && playerPoints > 21 ) {
-      alert('Computadora gano :( :(');
-    } else {
-      // console.log( 'test', { computerPotins, playerPoints });
-      alert('Nadie ha ganado >_<');
-    }
-  }, 500 )
+    do {
 
-}
+      const card = requestCard();
+      const playerTurn = playersPoints.length - 1;
+      computerPotins = accumulatePoints(card, playerTurn);
+      createCard(card, playerTurn );
 
-/* ------------------ Eventos ------------------ */
-// Se agrega un evento a cada botón utilizando el método addEventListener.
-// Este método escucha un evento específico (en este caso, 'click') y ejecuta una función (callback) cuando ocurre dicho evento.
+    } while ( (computerPotins < minPoints) && (minPoints <= 21) );
 
-/* ------ Evento: Pedir Carta ------ */
-btnRequest.addEventListener('click', () => {
+    determineWinner();
 
-  const card = requestCard();
-  playerPoints = playerPoints + cardValue( card );
-  txtPoints[0].innerText = playerPoints;
-
-  const imgCard = document.createElement('img');
-  imgCard.src = `assets/cartas/${ card }.png`;
-  imgCard.classList.add('cards');
-
-  divPlayerCards.append( imgCard );
-
-  if ( playerPoints > 21 ) {
-    // console.warn( 'Perdistes :(' );
-    btnRequest.disabled = true;
-    btnStop.disabled = true;
-    computerTurn( playerPoints );
-  } else if ( playerPoints === 21 ) {
-    // console.warn( '21, genial!' );
-    btnRequest.disabled = true;
-    btnStop.disabled = true;
-    computerTurn( playerPoints );
   }
 
-});
+  /* ------ Evento: Pedir Carta ------ */
+  btnRequest.addEventListener('click', () => {
 
-/* ------ Evento: Turno conputadora ------ */
-btnStop.addEventListener('click', () => {
-  btnRequest.disabled = true;
-  btnStop.disabled = true;
+    const card = requestCard();
+    const playerTurn = 0;
+    const playerPoints = accumulatePoints(card, playerTurn)
+    createCard(card, playerTurn);
 
-  computerTurn( playerPoints );
-});
 
-/* ------ Evento: Nuevo Juego ------ */
-btnNew.addEventListener('click', () => {
-  deck = [];
-  createDeck();
-  playerPoints = 0;
-  computerPotins = 0;
+    if ( playerPoints > 21 ) {
+      // console.warn( 'Perdistes :(' );
+      btnRequest.disabled = true;
+      btnStop.disabled = true;
+      computerTurn( playerPoints );
+    } else if ( playerPoints === 21 ) {
+      // console.warn( '21, genial!' );
+      btnRequest.disabled = true;
+      btnStop.disabled = true;
+      computerTurn( playerPoints );
+    }
 
-  txtPoints[0].innerText = playerPoints;
-  txtPoints[1].innerText = computerPotins;
+  });
 
-  divPlayerCards.innerHTML = '';
-  divComputerCards.innerHTML = '';
+  /* ------ Evento: Turno conputadora ------ */
+  btnStop.addEventListener('click', () => {
+    btnRequest.disabled = true;
+    btnStop.disabled = true;
 
-  btnRequest.disabled = false;
-  btnStop.disabled = false;
+    computerTurn( playersPoints[0] );
+  });
 
-});
+  /* ------ Evento: Nuevo Juego ------ */
+  btnNew.addEventListener('click', () => {
+    startGame();
+  });
+
+  return {
+    newGame: startGame
+  }
+
+})();
